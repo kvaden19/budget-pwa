db59d42575bc1e8f8c6f518e377aeeee24deb813
Kelly Vaden <klvaden@gmail.com>
2021-06-13 10:00:20 -0400

    Set up service worker to pre-cache static files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,17 @@
-console.log("Hi from your service-worker.js file!");
+// Is this step necessary for the budget app?
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './styles.css',
+];
+
+// Install and pre-cache static files
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(FILES_TO_CACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // Activate service worker immediately once it has finished installing
+    self.skipWaiting();
+});
